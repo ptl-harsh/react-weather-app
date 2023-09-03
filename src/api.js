62adee69e1a2a4f90b475563a23959a1a743fab3
ptl-harsh <119760008+ptl-harsh@users.noreply.github.com>
2023-09-03 13:55:41 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+	  "X-RapidAPI-Key": "267f007348mshe99243ea8c2a72cp1c0be6jsn74fedc24e751",// rapid api key 
+	  "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "b990b12f439d0dc922dc7fddf6dd579f"; //  key from openweather API
